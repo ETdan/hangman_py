d956ccf46c354f53eb648d70173cb90ff1f8e8b7
Daniel Asfaw <asfawdaniel779@gmail.com>
2023-08-01 04:13:52 -0500

    first commit

---
+++ words.py
@@ -0,0 +1,2 @@
+words=["camel","advard","baboon","beach","mountain","forest","space","castle","superhero","fruit","animal","sports","music","food","vehicle","occupation","mythology","technology","countries","movies","books","colors","emotions"]
+gussedrwords=[]
